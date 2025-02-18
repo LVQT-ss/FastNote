0a50d9873d8d3efd452a0be57fbfb4a0a2fbdde7
LVQT <levietquocthinh@gmail.com>
2025-02-19 01:56:59 +0700

    update security

--- be/server.js
+++ be/server.js
@@ -1,4 +1,5 @@
-// File: server.js
+require('dotenv').config(); // Load environment variables
+
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -6,13 +7,14 @@ const noteRoutes = require('./routes/noteRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI; // Get MongoDB URI from .env file
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://thinhlvqse171791:nb0rBIwX85ITPhTF@cluster0.haymn.mongodb.net/", {
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
